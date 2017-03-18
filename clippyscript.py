6dec6ebac6b5efce92509bcdbea345086e9a29f3
Albert Brox III <albert@exypno.tech>
2017-03-18 17:37:11 -0400

    removed unnecessary str()

--- clippyscript.py
+++ clippyscript.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     import sys
 
     r = randint(0,3)
-    message0 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + str(sys.argv[1]) + "\n\nWould you like to install Pulse Audio?")
-    message1 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + str(sys.argv[1]) + "\n\nWould you like to delete the root directory?")
-    message2 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + str(sys.argv[1]) + "\n\nWould you like help writing a letter?")
-    message3 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + str(sys.argv[1]) + "\n\nWould you like me to reboot your computer?")
+    message0 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to install Pulse Audio?")
+    message1 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to delete the root directory?")
+    message2 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like help writing a letter?")
+    message3 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to reboot your computer?")
 
     if r == 0:
         call(['cowsay', '-fclippy', message0])
     elif r == 1:
-        call(['cowsay', '-fclippy', message1])
+        call(['cowsay', '-fclippy', message1]
     elif r == 2:
         call(['cowsay', '-fclippy', message2])
     elif r == 3:
