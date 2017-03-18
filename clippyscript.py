030bcfc2c253dcc275638e14462c5d9a89ba2824
Albert Brox III <albert@exypno.tech>
2017-03-18 19:58:29 -0400

    fixed weird phrasing

--- clippyscript.py
+++ clippyscript.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     import sys
 
     r = randint(0,3)
-    message0 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to install Pulse Audio?")
-    message1 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to delete the root directory?")
-    message2 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like help writing a letter?")
-    message3 = ("Hi, I am Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to reboot your computer?")
+    message0 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to install Pulse Audio?")
+    message1 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to delete the root directory?")
+    message2 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like help writing a letter?")
+    message3 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to reboot your computer?")
 
     if r == 0:
         call(['cowsay', '-fclippy', message0])
