1504e5816808f23d98dcef3e373583ee2b7b78ed
Albert Brox III <albert@exypno.tech>
2017-03-18 21:33:59 -0400

    added more messages, fixed seed

--- clippyscript.py
+++ clippyscript.py
@@ -2,17 +2,19 @@
 
 from subprocess import call
 from random import randint, seed
-from time import ctime # for seeding
+from time import time # for seeding
 
 if __name__ == "__main__":
-    seed(ctime())
+    seed(time())
     import sys
 
-    r = randint(0,3)
+    r = randint(0,4)
+
     message0 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to install Pulse Audio?")
     message1 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to delete the root directory?")
     message2 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like help writing a letter?")
     message3 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to reboot your computer?")
+    message4 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to pipe myself to /dev/null?")
 
     if r == 0:
         call(['cowsay', '-fclippy', message0])
@@ -22,3 +24,5 @@ if __name__ == "__main__":
         call(['cowsay', '-fclippy', message2])
     elif r == 3:
         call(['cowsay', '-fclippy', message3])
+    elif r == 4:
+        call(['cowsay', '-fclippy', message4])
