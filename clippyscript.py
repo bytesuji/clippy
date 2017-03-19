28af0fe6406b3fc0f08e16a172d007477f409f6d
Albert Brox III <albert@exypno.tech>
2017-03-18 23:54:12 -0400

    added more smug messages

--- clippyscript.py
+++ clippyscript.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     seed(time())
     import sys
 
-    r = randint(0,4)
+    r = randint(0,5)
 
     message0 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to install Pulse Audio?")
     message1 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like to delete the root directory?")
     message2 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like help writing a letter?")
     message3 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to reboot your computer?")
     message4 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to pipe myself to /dev/null?")
+    message5 = ("Hi, I'm Clippy, your shell assisstant. Command not found:\n\n" + sys.argv[1] + "\n\nWould you like me to disable your keyboard?")
 
     if r == 0:
         call(['cowsay', '-fclippy', message0])
@@ -26,3 +27,5 @@ if __name__ == "__main__":
         call(['cowsay', '-fclippy', message3])
     elif r == 4:
         call(['cowsay', '-fclippy', message4])
+    elif r == 5:
+        call(['cowsay', '-fclippy', message5])
